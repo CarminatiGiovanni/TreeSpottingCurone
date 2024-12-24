8704716fc57d9945b3723f7213cb411c16efbb41
Hey JOe <jocarmi03@gmail.com>
2024-12-24 16:54:44 +0100

    index.js good

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const PORT = process.env.PORT || 80;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
-    // res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    // res.send('Hello World');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
     console.log(`>> Listening at PORT:${PORT}`);
 });
 
-// 54.86.60.29:3000
+// 54.86.60.29:80
 // ec2-user
 //  kill -9 $(ps aux | grep '\snode\s' | awk '{print $2}')
